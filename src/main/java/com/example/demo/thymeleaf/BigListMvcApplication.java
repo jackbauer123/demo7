681b68782d01393448c44e65425bb431d69523c5
jackbauer123 <251908410@qq.com>
2023-01-31 11:16:37 +0800

    push

---
+++ src/main/java/com/example/demo/thymeleaf/BigListMvcApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigListMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigListMvcApplication.class, args);
+	}
+}
